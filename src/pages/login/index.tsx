4923fea69b3094d5ce7913babaa98d7b5eaa60e9
KangDroid <hyunwoo286@gmail.com>
2023-01-24 17:37:08 +0900

    [Feature] Handle login response

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,53 @@
 import { FolderOpenTwoTone } from '@ant-design/icons';
-import { CodeResponse, useGoogleLogin } from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
 import { Button } from 'antd';
 import Title from 'antd/es/typography/Title';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { AuthenticationProvider } from '../../apis';
+import { accountApi } from '../../App';
+import { JoinTokenHelper } from '../../helpers/joinTokenHelper';
+import { LocalStorageHelper } from '../../helpers/localStorageHelper';
 
 function LoginPage() {
-  const onGoogleLoginSuccess = (oAuthCodeResponse: CodeResponse) => {
-    console.log(oAuthCodeResponse);
+  const navigate = useNavigate();
+
+  // Common OAuth Login Handler
+  async function handleOAuthAccount(provider: AuthenticationProvider, oAuthCode: string) {
+    try {
+      const response = await accountApi.loginAccount({
+        authenticationProvider: provider,
+        authCode: oAuthCode
+      });
+
+      LocalStorageHelper.setItem('accessToken', response.data.accessToken);
+      LocalStorageHelper.setItem('refreshToken', response.data.refreshToken);
+    } catch (error) {
+      handleLoginError(error);
+    }
+  }
+
+  // Login Error Handler(redirecting to join)
+  const handleLoginError = (error: unknown) => {
+    // Case 1. Error is not axios error.
+    if (!axios.isAxiosError(error)) throw error;
+
+    // Case 2. OAuth user does not exists.
+    if (error.response?.status === 404) {
+      // 1. Parse Join Token
+      const { joinToken } = error.response.data;
+      const joinTokenParsed = JoinTokenHelper.createJoinTokenFromJwt(joinToken);
+
+      // 2. Navigate to join page.(TODO: Properly create join page)
+      navigate('/', { state: joinTokenParsed });
+    }
   };
 
   // Setup Google Login Hook
   const googleLoginHook = useGoogleLogin({
-    onSuccess: onGoogleLoginSuccess,
+    onSuccess: (oAuthCodeResponse) =>
+      handleOAuthAccount(AuthenticationProvider.Google, oAuthCodeResponse.code),
     flow: 'auth-code'
   });
 
