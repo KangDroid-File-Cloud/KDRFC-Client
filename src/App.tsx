d538f8c76b6c20e769c7da171d3e26766479dace
KangDroid <hyunwoo286@gmail.com>
2023-02-18 22:50:44 +0900

    [Improvement] Track API Base URL in .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
 import { AccountApi, Configuration } from './apis';
+import { FILE_API_BASE_URL } from './configs/GlobalConfig';
 import GoogleOAuthCallback from './pages/auth/redirect/google';
 import KakaoOAuthCallback from './pages/auth/redirect/kakao';
 import JoinPage from './pages/join';
@@ -9,7 +10,7 @@ import LoginWrapperPage from './pages/login/wrappter';
 // Axios/API Init Area
 const axiosInstance = axios.create();
 const apiConfiguration = new Configuration({
-  basePath: 'http://localhost:5096'
+  basePath: FILE_API_BASE_URL
 });
 
 export const accountApi = new AccountApi(apiConfiguration, undefined, axiosInstance);
--- src/configs/GlobalConfig.ts
+++ src/configs/GlobalConfig.ts
@@ -1,2 +1,3 @@
 export const GOOGLE_OAUTH_ID = import.meta.env.VITE_GOOGLE_OAUTH_ID;
 export const KAKAO_OAUTH_ID = import.meta.env.VITE_KAKAO_OAUTH_ID;
+export const FILE_API_BASE_URL = import.meta.env.VITE_API_SERVER_URL;
