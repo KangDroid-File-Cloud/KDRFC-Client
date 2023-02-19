c55271c4fd8064192e7fa8b8e7e117c8099bcb3d
KangDroid <hyunwoo286@gmail.com>
2023-02-19 16:49:49 +0900

    [Feature] Add support Self Credential Provider Login (#4)
    * [Feature] Add support Self Credential Provider Login
    
    * [Improvement] Redirect to root page when self-login succeeeds
    
    * [Chore] Apply Lint
    
    ---------
    
    Co-authored-by: KangDroid <kangdroid@outlook.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { FILE_API_BASE_URL } from './configs/GlobalConfig';
 import GoogleOAuthCallback from './pages/auth/redirect/google';
 import KakaoOAuthCallback from './pages/auth/redirect/kakao';
 import JoinPage from './pages/join';
+import SelfLoginPage from './pages/login/self';
 import LoginWrapperPage from './pages/login/wrappter';
 
 // Axios/API Init Area
@@ -19,6 +20,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginWrapperPage />} />
+      <Route path="/login/self" element={<SelfLoginPage />} />
       <Route path="/join" element={<JoinPage />} />
       <Route path="/auth/redirect/google" element={<GoogleOAuthCallback />} />
       <Route path="/auth/redirect/kakao" element={<KakaoOAuthCallback />} />
